8c88cf2b916b290f2a79b5903ab4fcf28377bc59
misco <ing.felipepadilla@gmail.com>
2019-03-28 11:49:50 -0500

    agrego proyecto

---
+++ app/src/main/java/com/example/prueba2/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.prueba2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageButton boton1 = findViewById(R.id.button1);
+
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                boton1.setImageResource(R.drawable.ic_circulo);
+
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                "Toast por defecto", Toast.LENGTH_SHORT);
+
+                toast1.show();
+
+
+            }
+        });
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
